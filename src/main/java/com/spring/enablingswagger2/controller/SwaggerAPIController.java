e77848fd646da4992c369603933810f0484b5088
samukma <nguyensam576@gmail.com>
2021-11-17 16:15:51 +0700

    done swagger2

---
+++ src/main/java/com/spring/enablingswagger2/controller/SwaggerAPIController.java
@@ -0,0 +1,23 @@
+package com.spring.enablingswagger2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class SwaggerAPIController {
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<String> getProducts() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public String createProduct() {
+        return "Product is saved successfully";
+    }
+}
---
+++ src/test/java/com/spring/enablingswagger2/EnablingSwagger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.enablingswagger2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnablingSwagger2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
